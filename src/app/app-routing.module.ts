3ea0114246e93056835553494c792dbfc5e8ed76
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2021-10-20 15:05:48 -0300

    Primeiro Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './page/contatos/contatos.component';
+import { HomeComponent } from './page/home/home.component';
+import { SobrenosComponent } from './page/sobrenos/sobrenos.component';
+import { TituloComponent } from './page/titulo/titulo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'titulo', component: TituloComponent},
+  {path: 'sobrenos', component: SobrenosComponent},
+  {path: 'contatos', component: ContatosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
